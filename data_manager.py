23a5929a020be9e7fefdea958f7f8ac591e362a7
Mmac3569 <matej.maca009@gmail.com>
2026-05-10 09:41:36 +0200

    create data on first start

--- data_manager.py
+++ data_manager.py
@@ -8,10 +8,14 @@ class DataManager():
         self.load_stats()
 
     def load_user_data(self):
-        file = open("./data/user.json", "r", encoding="utf-8")
-        data = json.load(file)
-        self.username = data["username"]
-        picture_path = "./data/" + data["profile-picture"]
+        try:
+            file = open("./data/user.json", "r", encoding="utf-8")
+            data = json.load(file)
+            self.username = data["username"]
+            picture_path = "./data/" + data["profile-picture"]
+        except:
+            self.username = "Anonymní uživatel"
+            picture_path = "./data/default_profile_picture.png"
         try:
             self.profile_picture = Image.open(picture_path)
         except:
@@ -31,16 +35,22 @@ class DataManager():
         file = open("./data/user.json", "w", encoding="utf-8")
         json.dump({
             "username": self.username,
-            "profile-picture": "profile_picture." + self.profile_picture.format
+            "profile-picture": "profile_picture." + self.profile_picture.format.lower()
         }, file, indent=4, ensure_ascii=False)
 
     def load_stats(self):
-        file = open("./data/stats.json", "r", encoding="utf-8")
-        data = json.load(file)
-        self.games_played = data["games_played"]
-        self.wins = data["wins"]
-        self.draws = data["draws"]
-        self.losses = data["losses"]
+        try:
+            file = open("./data/stats.json", "r", encoding="utf-8")
+            data = json.load(file)
+            self.games_played = data["games_played"]
+            self.wins = data["wins"]
+            self.draws = data["draws"]
+            self.losses = data["losses"]
+        except:
+            self.games_played = 0
+            self.wins = 0
+            self.draws = 0
+            self.losses = 0
     
     def save_stats(self, games_played = None, wins = None, draws = None, losses = None):
         if games_played == None and wins == None and draws == None and losses == None:
